de01aa03a1f28614618f415d4e95bcf09ef4b224
Toshichika Mashimo <mashita1023@gmail.com>
2022-01-13 06:20:56 +0900

    create database and DDL

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 A simple command-line application.
+
+# Run
+
+``` fish
+$ docker-compose build
+$ docker-comopose up -d
+$ /bin/sh scripts/script.sh
+$ mysqldef -umysql precomm < sql/<name>.create.sql
+$ dart run
+```
+
--- bin/app.dart
+++ bin/app.dart
@@ -1,9 +1,14 @@
 import '../internal/di/di.dart';
+import '../internal/external/mysql/mysql.dart';
 
 void main() async {
   final server = initializeServer();
-  await server.run();
+  final mysql = Mysql();
+  var test = mysql.start();
+  print(test);
+  
+//  await server.run();
 
-  print(server.server.runtimeType);
+//  print(server.server.runtimeType);
 }
 
